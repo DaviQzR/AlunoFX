5b0094409270aeb9571358b1b5d5a13f27ad103d
DaviQzR <125469425+DaviQzR@users.noreply.github.com>
2023-11-03 11:56:34 -0300

    Ajuste no Nascimento

--- src/View/AlunoCrud.java
+++ src/View/AlunoCrud.java
@@ -2,6 +2,7 @@ package View;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import Model.Aluno;
@@ -17,6 +18,7 @@ import javafx.stage.Stage;
 public class AlunoCrud extends Application {
     private List<Aluno> alunos = new ArrayList<>();
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("ddMMyyyy"); // Formato de entrada alternativo
 
     public static void main(String[] args) {
         launch(args);
@@ -34,7 +36,7 @@ public class AlunoCrud extends Application {
         Label lblRa = new Label("RA:");
         TextField txtRa = new TextField();
 
-        Label lblNascimento = new Label("Nascimento :");
+        Label lblNascimento = new Label("Nascimento:"); 
         TextField txtNascimento = new TextField();
 
         Button btnAdicionar = new Button("Adicionar");
@@ -55,15 +57,15 @@ public class AlunoCrud extends Application {
 
         lblNascimento.setLayoutX(20);
         lblNascimento.setLayoutY(100);
-        txtNascimento.setLayoutX(120);
+        txtNascimento.setLayoutX(120); // Ajusta a posição do campo de data de nascimento
         txtNascimento.setLayoutY(100);
-        
+
         btnAdicionar.setLayoutX(20);
         btnAdicionar.setLayoutY(140);
 
         btnPesquisar.setLayoutX(120);
         btnPesquisar.setLayoutY(140);
-        
+
         resultadoPesquisa.setLayoutX(20);
         resultadoPesquisa.setLayoutY(180);
         resultadoPesquisa.setPrefWidth(300);
@@ -73,12 +75,26 @@ public class AlunoCrud extends Application {
             String nome = txtNome.getText();
             int ra = Integer.parseInt(txtRa.getText());
             String dataNascimento = txtNascimento.getText();
-            LocalDate nascimento = LocalDate.parse(dataNascimento, formatter);
-            Aluno aluno = new Aluno(alunos.size() + 1, ra, nome, nascimento);
-            alunos.add(aluno);
-            txtNome.clear();
-            txtRa.clear();
-            txtNascimento.clear();
+            
+            try {
+                LocalDate nascimento = LocalDate.parse(dataNascimento, formatter); // Tenta analisar no formato padrão
+                Aluno aluno = new Aluno(alunos.size() + 1, ra, nome, nascimento);
+                alunos.add(aluno);
+                txtNome.clear();
+                txtRa.clear();
+                txtNascimento.clear();
+            } catch (DateTimeParseException e) {
+                try {
+                    LocalDate nascimento = LocalDate.parse(dataNascimento, inputFormatter); // Tenta analisar no formato alternativo
+                    Aluno aluno = new Aluno(alunos.size() + 1, ra, nome, nascimento);
+                    alunos.add(aluno);
+                    txtNome.clear();
+                    txtRa.clear();
+                    txtNascimento.clear();
+                } catch (DateTimeParseException ex) {
+                    resultadoPesquisa.setText("Formato de data inválido. Use o formato ddMMyyyy ou dd/MM/yyyy.");
+                }
+            }
         });
 
         btnPesquisar.setOnAction(event -> {
